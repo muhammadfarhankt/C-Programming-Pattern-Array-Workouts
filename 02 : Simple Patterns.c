a7f6852558c6e80ffcf7ec4628f7b69fa43bf32e
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-10-04 22:55:10 +0530

    Create 02 : Simple Patterns.c

---
+++ 02 : Simple Patterns.c
@@ -0,0 +1,53 @@
+Video 2: Simple Patterns
+
+Code in VS Code and stop execution by line by line for understanding
+Concept of rows and Columns
+Explain 2D matrix
+
+Square Star pattern 
+*  *  *  *  
+*  *  *  *  
+*  *  *  *  
+*  *  *  *
+  
+Increasing Pattern
+*
+* *
+* * *
+* * * *
+* * * * * 
+Increasing by double 
+* * 
+* * * *
+* * * * * *
+* * * * * * * * 
+  
+H/W 1:  Increase by triple (multiples of 3)
+
+Star Pattern decreasing
+
+Explain concepts of (- -) decrement operator, >= in condition etc (if <= used which lead to infinite loop)
+
+* * * * 
+* * * 
+* *
+*
+
+H/W 2:  (Decrementing by 2)
+
+*  *  *  *  *  *  *  *  
+*  *  *  *  *  *  
+*  *  *  *  
+*  *
+  
+H/W 3 (Explain rows till center then another loop for decrementing)
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
