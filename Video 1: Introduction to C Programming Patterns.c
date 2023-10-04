9826a721bce5684814c6315aaf95fd0974cb5b91
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-10-04 22:52:41 +0530

    Create Video 1: Introduction to C Programming Patterns.c

---
+++ Video 1: Introduction to C Programming Patterns.c
@@ -0,0 +1,16 @@
+// Concept of patterns in programming
+// Importance of patterns, their applications, and how they can enhance problem-solving skills. 
+// Pre-requisites : Basic C Programming, Data types, If else conditions, For loop, etc
+// 100k coding challenge (5 parts) / 3 Hour Coding full
+// Patterns help us solve complex problems by breaking them down into smaller, more manageable components.
+// They provide structure, organization, and efficiency to our code. By learning patterns, you'll develop problem-solving skills that are essential for any programmer.
+// To follow along with this series, you'll need a C development environment. There are various options available, such as IDEs (Integrated Development Environments) like Code::Blocks, Dev-C++, or text editors like Notepad++ or Visual Studio Code. Choose the one that suits your preferences and set it up on your machine.
+// I choose online gdb
+// Simple patterns 1, 2, 3, … & 1, 3, 5, 7, …
+// H/W 1, 5, 9, … 
+// 100, 99, 98, … 1 & 99, 95, 91, … 2
+// Print multiplication table of a number i/p from user
+// H/W : Learn 2D matrix and concepts of rows and columns & Nested for loops
+
+// https://www.geeksforgeeks.org/pattern-programs-in-c/ 
+// https://www.javatpoint.com/star-program-in-c 
