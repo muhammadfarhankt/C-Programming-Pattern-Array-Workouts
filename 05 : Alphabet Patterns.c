bbf567bb1a38053758f76a80de0c692ace20dc9d
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-10-04 22:59:17 +0530

    Create 05 : Alphabet Patterns.c

---
+++ 05 : Alphabet Patterns.c
@@ -0,0 +1,32 @@
+5: Alphabet Patterns
+Concepts ASCII 
+a - 97
+A - 65
+
+A 
+A  B 
+A  B  C 
+A  B  C  D 
+A  B  C  D  E  
+
+A
+1  1
+B  B   B
+2   2   2  2
+
+H/W 9
+
+A
+A   2 
+A   2   C
+A   2   C  4
+
+HW 10
+
+A
+A  1  
+A  1  B
+A  1  B  2
+
+Concept of input a string from user and find it’s length
+
