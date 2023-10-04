312b6e6a3b61098a6491e941f2c81827b1a9205e
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-10-04 22:56:30 +0530

    Create 03 : Basic Number patterns.c

---
+++ 03 : Basic Number patterns.c
@@ -0,0 +1,60 @@
+Video 3: Basic Number patterns
+Explain again concepts of row and columns
+
+Basic column number pattern
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+H/W 3: Row number pattern
+Explain row concept again
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+
+1  
+1  2  
+1  2  3  
+1  2  3  4  
+1  2  3  4  5  
+
+H/W 4: 
+1  
+2  2  
+3  3  3  
+4  4  4  4  
+5  5  5  5  5  
+
+
+Decreasing Number Pattern
+5  
+4  5  
+3  4  5  
+2  3  4  5  
+1  2  3  4  5  
+
+H/W 5: 
+5  
+5  4  
+5  4  3  
+5  4  3  2  
+5  4  3  2  1  
+
+H/W 6: Explain 
+1  
+1  2  
+1  2  3  
+1  2  3  4  
+1  2  3  4  5  
+1  2  3  4  
+1  2  3  
+1  2  
+1  
+
+
+
+
