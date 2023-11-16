38d337471b71de5e93e167f7967a1db4937462fc
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-11-16 23:10:31 +0530

    Update 002 Find the average of n numbers.c

--- Array/002 Find the average of n numbers.c
+++ Array/002 Find the average of n numbers.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main() {
-   int a[100];
-   int size,i;
+   int num[20],size,i;
    float sum=0,avg;
    printf("Enter Array Size : ");
    scanf("%d",&size);
    printf("Enter the elements : ");
    for(i=0;i<size;i++){
-        scanf("%d",&a[i]);
-        sum=sum+a[i];
+        scanf("%d",&num[i]);
+        sum=sum+num[i];
    }
    avg=sum/size;
    printf("Average of the number is : %f",avg);
