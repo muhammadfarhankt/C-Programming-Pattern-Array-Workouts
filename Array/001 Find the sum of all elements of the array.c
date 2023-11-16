f69375ba4df384abeb7428a97c43ea639ce2ae5d
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-11-16 23:05:50 +0530

    size instead of n

--- Array/001 Find the sum of all elements of the array.c
+++ Array/001 Find the sum of all elements of the array.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
  int a[100];
- int i,n,j,sum=0;
+ int i,size,j,sum=0;
  printf("Enter the size of array:");
- scanf("%d",&n);
+ scanf("%d",&size);
  printf("Enter the elements:\n");
- for(i=0;i<n;i++){
+ for(i=0;i<size;i++){
      scanf("%d",&a[i]);
  }
- for(j=0;j<n;j++){
+ for(j=0;j<size;j++){
      sum=sum+a[j];
  }
  printf("The sum of elements are %d",sum);
@@ -16,7 +16,7 @@ return 0;
 }
 
 // Using one loop
-// for(i=0;i<n;i++){
+// for(i=0;i<size;i++){
 //      scanf("%d",&a[i]);
 //      sum=sum+a[j];
 //  }
