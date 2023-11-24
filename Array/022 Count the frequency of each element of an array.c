be33bccf303411374673c080bd6c6a44ccf97c18
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-11-25 00:27:45 +0530

    Create 022 Count the frequency of each element of an array.c

---
+++ Array/022 Count the frequency of each element of an array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], i, j, count = 1, size;
+
+    // Input size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    // Input elements of the array
+    printf("Enter the elements: ");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Loop to find and print the frequency of each unique element
+    for (i = 0; i < size; i++) {
+        count = 0; // Reset count for each new element
+        for (j = 0; j < size; j++) {
+            // Check if the current element matches any other element
+            if (arr[i] == arr[j]) {
+                // If the current element is found at an earlier index, skip counting
+                if (i < j) {
+                    count--;
+                    break;
+                }
+                count++; // Increment count for each occurrence of the element
+            }
+        }
+        // Print the frequency of each unique element
+        if (count > 0) {
+            printf("\nFrequency of %d is %d.", arr[i], count);
+        }
+    }
+
+    return 0;
+}
