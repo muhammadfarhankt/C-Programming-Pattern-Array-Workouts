493042c09b0c8ecf24f9eeb33736f2f9b1ba2125
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-23 21:39:04 +0530

    Update 02 : Simple Patterns.c

--- Pattern/02 : Simple Patterns.c
+++ Pattern/02 : Simple Patterns.c
@@ -10,18 +10,50 @@ Square Star pattern
 *  *  *  *  
 *  *  *  *
   
-Increasing Pattern
+// Increasing Pattern
 *
 * *
 * * *
 * * * *
 * * * * * 
-Increasing by double 
+  
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter Number of rows : ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// Increasing by double 
 * * 
 * * * *
 * * * * * *
 * * * * * * * * 
-  
+
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter Number of rows : ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= 2 * i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
 H/W 1:  Increase by triple (multiples of 3)
 
 Star Pattern decreasing
@@ -33,12 +65,44 @@ Explain concepts of (- -) decrement operator, >= in condition etc (if <= used wh
 * *
 *
 
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter Number of rows : ");
+    scanf("%d", &rows);
+
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
 H/W 2:  (Decrementing by 2)
 
 *  *  *  *  *  *  *  *  
 *  *  *  *  *  *  
 *  *  *  *  
 *  *
+
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter Number of rows : ");
+    scanf("%d", &rows);
+
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= 2 * i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
   
 H/W 3 (Explain rows till center then another loop for decrementing)
 
@@ -51,3 +115,28 @@ H/W 3 (Explain rows till center then another loop for decrementing)
 * * *
 * *
 *
+
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter Number of rows : ");
+    scanf("%d", &rows);
+
+    // Upper half of the pattern
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // Lower half of the pattern
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
