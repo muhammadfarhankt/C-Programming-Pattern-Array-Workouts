a2c2cc60bfa97bda4a6091f6c377207e1521f2e4
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-23 21:46:37 +0530

    Update 03 : Basic Number patterns.c

--- Pattern/03 : Basic Number patterns.c
+++ Pattern/03 : Basic Number patterns.c
@@ -8,6 +8,23 @@ Basic column number pattern
 1 2 3 4 5
 1 2 3 4 5
 
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
 H/W 3: Row number pattern
 Explain row concept again
 1 1 1 1 1
@@ -16,18 +33,69 @@ Explain row concept again
 4 4 4 4 4
 5 5 5 5 5
 
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+Pattern with incrementing numbers
 1  
 1  2  
 1  2  3  
 1  2  3  4  
 1  2  3  4  5  
 
-H/W 4: 
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+H/W 4: Pattern with incrementing rows and constant columns
 1  
 2  2  
 3  3  3  
 4  4  4  4  
 5  5  5  5  5  
+
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
 
 Decreasing Number Pattern
@@ -36,15 +104,50 @@ Decreasing Number Pattern
 3  4  5  
 2  3  4  5  
 1  2  3  4  5  
+  
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
 
-H/W 5: 
+    for (int i = rows; i >= 1; i--) {
+        for (int j = rows; j >= i; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+  
+
+H/W 5: Decreasing row number pattern
 5  
 5  4  
 5  4  3  
 5  4  3  2  
 5  4  3  2  1  
+  
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
 
-H/W 6: Explain 
+    for (int i = 1; i <= rows; i++) {
+        for (int j = rows; j >= i; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+H/W 6: Pattern with incrementing rows and columns (left-aligned)
 1  
 1  2  
 1  2  3  
@@ -55,6 +158,28 @@ H/W 6: Explain
 1  2  
 1  
 
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
 
 
